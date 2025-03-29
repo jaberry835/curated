edde7dd81fa969790a95e18f432c9d2c3d322c57
Jack Berry <jaberry@microsoft.com>
2025-03-29 11:33:17 -0400

    made a bunch of changes

--- angular-spa/src/app/app-insights-service.ts
+++ angular-spa/src/app/app-insights-service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@angular/core";
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 import { environment } from "../environments/environment";
+import { MsalService } from "@azure/msal-angular";
 @Injectable({
   providedIn: "root",
 })
 export class AppInsightsService {
   private appInsights: ApplicationInsights;
 
-  constructor() {
+  constructor(private authService: MsalService) {
     this.appInsights = new ApplicationInsights({
       config: {
         instrumentationKey: environment.insightsKey, // Replace with your Azure App Insights key
@@ -17,6 +18,10 @@ export class AppInsightsService {
   }
 
   logCustomEvent(eventName: string, properties?: { [key: string]: any }) {
+    const account = this.authService.instance.getAllAccounts()[0];
+    const name = account.username;
+    
+    this.appInsights.setAuthenticatedUserContext(name, account.idTokenClaims?.upn || name, true);
     this.appInsights.trackEvent({ name: eventName, properties });
   }
 }
