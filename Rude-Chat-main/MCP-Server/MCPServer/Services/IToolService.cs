bacb33d9ebfc8b9a93ba4fefa3a1793c81681f11
Jack Berry <jaberry@microsoft.com>
2025-07-08 11:48:37 -0400

    cleaning repo

--- Rude-Chat-main/MCP-Server/MCPServer/Services/IToolService.cs
+++
@@ -1,9 +0,0 @@
-using MCPServer.Models;
-
-namespace MCPServer.Services;
-
-public interface IToolService
-{
-    Task<IEnumerable<McpTool>> GetAvailableToolsAsync();
-    Task<McpToolCallResponse> ExecuteToolAsync(McpToolCallRequest request, string? userToken = null);
-}
