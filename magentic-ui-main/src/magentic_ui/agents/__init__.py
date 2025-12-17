104e0ba10d0eb64cd993db7f7c81257c11cce0d7
Jack Berry <jaberry@microsoft.com>
2025-12-17 08:23:19 -0500

    Add/update magentic-ui (main)

---
+++ magentic-ui-main/src/magentic_ui/agents/__init__.py
@@ -0,0 +1,13 @@
+from .web_surfer import WebSurfer, WebSurferCUA, FaraWebSurfer
+from ._coder import CoderAgent
+from ._user_proxy import USER_PROXY_DESCRIPTION
+from .file_surfer import FileSurfer
+
+__all__ = [
+    "WebSurfer",
+    "WebSurferCUA",
+    "FaraWebSurfer",
+    "CoderAgent",
+    "USER_PROXY_DESCRIPTION",
+    "FileSurfer",
+]
